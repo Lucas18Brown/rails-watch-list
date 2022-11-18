1a3e6b24447888eed05c23def4e6d2dffd97de61
lucas brown <lucas20brown@hotmail.co.uk>
2022-11-18 16:53:13 +0000

    added styling and delete button

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: %i[new create]
+  before_action :set_bookmark, only: %i[destroy]
 
   def new
     @bookmark = Bookmark.new
@@ -15,10 +16,19 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(bookmark.list), status: :see_other
+  end
+
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def set_list
